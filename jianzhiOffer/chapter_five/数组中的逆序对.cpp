ca61443ea6981b3d6eb19542d9f1e0df5694cf07
water_rider <jzjswq@126.com>
2021-03-28 14:54:53 +0800

    update 51_InversePairs

--- jianzhiOffer/chapter_five/数组中的逆序对.cpp
+++ jianzhiOffer/chapter_five/数组中的逆序对.cpp
@@ -51,14 +51,67 @@ int InversePairs(int *data, int length)
 	return count;
 }
 
+int reverseCount = 0;
+
+void merge(int *nums, int start, int mid, int end)
+{
+	int *temp = new int[end - start + 1];
+	int p1 = start;
+	int p2 = mid + 1;
+	int p = 0;
+
+	while (p1 <= mid && p2 <= end)
+	{
+		if (nums[p1] <= nums[p2])
+		{
+			temp[p++] = nums[p1++];
+		}
+		else
+		{
+			reverseCount += mid - p1 + 1;
+			temp[p++] = nums[p2++];
+		}
+	}
+
+	while (p1 <= mid)
+		temp[p++] = nums[p1++];
+
+	while (p2 <= end)
+		temp[p++] = nums[p2++];
+
+	for (int i = 0; i < p; i++)
+		nums[start + i] = temp[i];
+
+	delete[] temp;
+}
+
+void mergeSort(int *nums, int start, int end)
+{
+	if (start < end)
+	{
+		int mid = start + ((end - start) >> 1);
+		mergeSort(nums, start, mid);
+		mergeSort(nums, mid + 1, end);
+		merge(nums, start, mid, end);
+	}
+}
+
 int main(int argc, const char *argv[])
 {
 	int arr[] = {7, 5, 6, 4};
+	constexpr int arrlen = sizeof(arr) / sizeof(int);
+
+	int arr_b[arrlen];
+	std::copy(arr, arr + arrlen, arr_b);
+
 	std::cout << "array: ";
 	for (auto &e : arr)
 		std::cout << e << ' ';
 
-	std::cout << "\nInverse count: " << InversePairs(arr, sizeof(arr) / sizeof(int)) << std::endl;
+	std::cout << "\nInverse count: " << InversePairs(arr, arrlen);
+
+	mergeSort(arr_b, 0, arrlen - 1);
+	std::cout << "\nInverse count: " << reverseCount << std::endl;
 
 	return 0;
 }
